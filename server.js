bd60781fff3e122d8290e87e4b4f8941962526e6
Nader <nadermahmoud513@gmail.com>
2025-07-25 20:03:41 +0300

    fix express server for railway

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // تأكد إن db.json موجود في نفس المجلد
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const express = require("express");
+const path = require("path");
 
-server.use(middlewares);
-server.use(router);
-server.listen(port, () => {
-  console.log("JSON Server is running on port", port);
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve the React build
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
