c1002a38dc8140e3fd468ceeea47e591158a30aa
Nader <nadermahmoud513@gmail.com>
2025-07-25 20:44:44 +0300

    serve build folder for deployment

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve the React build
+// Serve static files from build folder
 app.use(express.static(path.join(__dirname, "build")));
 
+// Handle all other routes with index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
